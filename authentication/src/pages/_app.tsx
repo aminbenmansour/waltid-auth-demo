701a79c18e4ddae71ff0cc0bc04559e4fc0c7ea5
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2023-02-14 19:12:39 +0100

    integrating rainbowkit

--- authentication/src/pages/_app.tsx
+++ authentication/src/pages/_app.tsx
@@ -1,6 +1,44 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum, polygonMumbai } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+const ALCHEMY_ID = process.env.ALCHEMY_ID
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, polygonMumbai],
+  [
+    alchemyProvider({ apiKey: 'txclrnZjqRr87kBYAP8j2Ptdn2dwPsOc'}),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Waltid Authentication',
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider coolMode chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
