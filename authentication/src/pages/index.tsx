c7ca9917f4968ece405aecb36057135d4c6238f8
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2023-02-15 00:26:37 +0100

    accessing the session server-side

--- authentication/src/pages/index.tsx
+++ authentication/src/pages/index.tsx
@@ -1,39 +1,46 @@
 import Head from "next/head";
-import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useAccount, useConnect } from "wagmi";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { getSession } from "next-auth/react";
+import { getToken } from "next-auth/jwt";
 
-const inter = Inter({ subsets: ["latin"] });
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 import Link from "next/link";
 
-export default function Home() {
-  const { connect, connectors } = useConnect();
-
-  return (
+export default function Home({ address }: AuthenticatedPageProps) {
+  return address ? (
     <>
-      <Head>
-        <title>Waltid Demo Application</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <div>
-            <ConnectButton />
-          </div>
-          
-          <h1>Welcome Onboard: </h1>
-          <ul>
-            <li>
-              <Link href="/profile">Check profile</Link>
-            </li>
-          </ul>
-        </div>
-      </main>
+      <ConnectButton />
+      <h1>Authenticated</h1>
+    </>
+  ) : (
+    <>
+      <ConnectButton />
+      <h1>Unauthenticated</h1>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+  const token = await getToken({ req: context.req });
+
+  const address = token?.sub ?? null;
+  // If you have a value for "address" here, your
+  // server knows the user is authenticated.
+
+  // You can then pass any data you want
+  // to the page component here.
+  return {
+    props: {
+      address,
+      session,
+    },
+  };
+};
+
+type AuthenticatedPageProps = InferGetServerSidePropsType<
+  typeof getServerSideProps
+>;
